73a2b42f9b948e9d9b61fc7011f264f85ae7485d
Ksumeyye <sumeeyyekaya@gmail.com>
2025-02-10 23:41:03 +0300

    Customer için Controller ve View tarafını tamamladım.

---
+++ Demo_Product/Controllers/CustomerController.cs
@@ -0,0 +1,48 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using Microsoft.AspNetCore.Mvc;
+using EntityLayer.Concrete;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
+
+
+namespace Demo_Product.Controllers
+{
+    public class CustomerController : Controller
+    {
+        CustomerManager customerManager = new CustomerManager(new EfCustomerDal());
+        public IActionResult Index()
+        {
+            var values = customerManager.TGetList();
+            return View(values);
+        }
+        [HttpGet]
+        public IActionResult AddCustomer()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult AddCustomer(Customer c)
+        {
+            customerManager.TInsert(c);
+            return RedirectToAction("Index");
+        }
+        public IActionResult DeleteCustomer(int id)
+        {
+            var values = customerManager.TGetById(id);
+            customerManager.TDelete(values);
+            return RedirectToAction("Index");
+        }
+        [HttpGet]
+        public IActionResult UpdateCustomer(int id)
+        {
+            var values = customerManager.TGetById(id);
+                return View(values);
+        }
+        [HttpPost]
+        public IActionResult UpdateCustomer(Customer c)
+        {
+            customerManager.TUpdate(c);
+            return RedirectToAction("Index");
+        }
+    }
+}
--- Demo_Product/Controllers/ProductController.cs
+++ Demo_Product/Controllers/ProductController.cs
@@ -1,7 +1,10 @@
 ﻿using BusinessLayer.Concrete;
+using BusinessLayer.FluentValidation;
 using DataAccessLayer.EntityFramework;
 using EntityLayer.Concrete;
 using Microsoft.AspNetCore.Mvc;
+//using System.ComponentModel.DataAnnotations;
+using FluentValidation.Results;
 
 namespace Demo_Product.Controllers
 {
@@ -21,7 +24,38 @@ namespace Demo_Product.Controllers
         [HttpPost]
         public IActionResult AddProduct(Product p)
         {
-            productManager.TInsert(p);
+            ProductValidator validationRules=new ProductValidator();
+            ValidationResult results = validationRules.Validate(p);
+            if (results.IsValid)
+            {
+                productManager.TInsert(p);
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                foreach(var item in results.Errors)
+                {
+                    ModelState.AddModelError(item.PropertyName, item.ErrorMessage);
+                }
+            }
+            return View();
+        }
+        public IActionResult DeleteProduct(int id)
+        {
+            var product = productManager.TGetById(id);
+            productManager.TDelete(product);
+            return RedirectToAction("Index");
+        }
+        [HttpGet]
+        public IActionResult UpdateProduct(int id)
+        {
+            var values = productManager.TGetById(id);
+            return View(values);
+        }
+        [HttpPost]
+        public IActionResult UpdateProduct(Product p)
+        {
+            productManager.TUpdate(p);
             return RedirectToAction("Index");
         }
     }
