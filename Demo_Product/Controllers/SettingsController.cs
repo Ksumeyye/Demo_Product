e659cf89386cb9df75d87016c5deb6ee1e62f026
Ksumeyye <sumeeyyekaya@gmail.com>
2025-04-08 23:51:37 +0300

    Login İşlemlerini tamamladım.

--- Demo_Product/Controllers/SettingsController.cs
+++ Demo_Product/Controllers/SettingsController.cs
@@ -1,11 +1,23 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using EntityLayer.Concrete;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
 
 namespace Demo_Product.Controllers
 {
     public class SettingsController : Controller
     {
-        public IActionResult Index()
+        private readonly UserManager<AppUser> _userManager;
+
+        public SettingsController(UserManager<AppUser> userManager) //SettingsController sınıfı üzerinden Consractor oluşturdum.
         {
+            _userManager = userManager;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Index()
+        {
+            var values = await _userManager.FindByIdAsync(User.Identity.Name); //User.Identity.Name ile giriş yapan kullanıcının bilgilerini alır.
             return View();
         }
     }
